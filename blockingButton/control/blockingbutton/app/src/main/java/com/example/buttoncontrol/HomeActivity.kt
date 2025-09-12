739c4cc8adfe447b46d707fee975c81d5714abff
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2025-09-12 18:00:40 +0800

    Create control app with no controls

---
+++ blockingButton/control/blockingbutton/app/src/main/java/com/example/buttoncontrol/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.buttoncontrol
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val signOutButton = findViewById<Button>(R.id.signOutButton)
+        signOutButton.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }
+    }
+}
